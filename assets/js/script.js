3f1537fb76da8aaf1379b0f490498646fb8864c8
noyon <noyon@gmail.com>
2024-12-07 15:46:26 +0600

    2/11

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,6 +34,19 @@
 		}
 	});
 
+	window.onload = function() {
+		$('.counter-value').each(function(){
+			$(this).prop('Counter',0).animate({
+				Counter: $(this).text()
+			},{
+				duration: 3500,
+				easing: 'swing',
+				step: function (now){
+					$(this).text(Math.ceil(now));
+				}
+			});
+		});
+	}
 		
 }(jQuery));
 
